bb64d02016313a5cb120052c7e1efdf5459d9eff
Yash728 <61221867+Yash728@users.noreply.github.com>
2020-08-30 00:12:22 +0530

    footer correction

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,10 +3,12 @@ import React from "react";
 function Footer() {
     // const year = new Date().getFullYear();
     return (
-        <footer>
-            <p>Contact in case of queries: <a href="https://github.com/yashSrivstav">Yash Srivastava</a></p>
-            <p>Created with ❤ and 🧠.</p>
-        </footer>
+        <div>
+            <footer>
+                <p>Contact in case of queries: <a href="https://github.com/yashSrivstav">Yash Srivastava</a></p>
+                <p>Created with ❤ and 🧠.</p>
+            </footer>
+        </div>
     );
 }
 
