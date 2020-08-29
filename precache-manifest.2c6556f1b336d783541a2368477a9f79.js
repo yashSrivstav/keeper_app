5f321569ec7a9dd1a85975b3ff7a03c7e5959ccf
Yash728 <61221867+Yash728@users.noreply.github.com>
2020-08-29 23:54:49 +0530

    Updates

---
+++ precache-manifest.2c6556f1b336d783541a2368477a9f79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f9b657d18ac5555947ad22393e6713d",
+    "url": "/keeper_app/index.html"
+  },
+  {
+    "revision": "07ec67a7bdccb990dd38",
+    "url": "/keeper_app/static/css/main.947b56f3.chunk.css"
+  },
+  {
+    "revision": "57672872cd5300357a0a",
+    "url": "/keeper_app/static/js/2.6c7b0e2f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/keeper_app/static/js/2.6c7b0e2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07ec67a7bdccb990dd38",
+    "url": "/keeper_app/static/js/main.8f4feb21.chunk.js"
+  },
+  {
+    "revision": "491076a2033ac053b9cc",
+    "url": "/keeper_app/static/js/runtime-main.2bb100e2.js"
+  }
+]);
